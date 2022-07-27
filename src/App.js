c579e99f2893d07a8651c43c24d2358b3d06bd25
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-27 11:31:19 +0300

    relace react router dom with a switch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import {Route} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import {Route, BrowserRouter,Switch} from 'react-router-dom';
 import AllMovies from './components/Pages/AllMovies';
 import FavoriteMovies from './components/Pages/FavoriteMovies';
 import NewMovies from './components/Pages/NewMovies';
 function App() {
   return (
-    <div>
-     <Route path='/' >
+    
+   <div>
+    <Switch>
+    <Route  exact path="/">
       <AllMovies />
-     </Route>
-     <Route path='/new-movies' >
-      <NewMovies />
-     </Route>
-     <Route path='/favorites' >
-      <FavoriteMovies/>
-     </Route>
-    </div>
+    </Route>
+    <Route path="/favorites">
+      <FavoriteMovies />
+    </Route>
+    <Route path="/new-movies">
+      <NewMovies/>
+    </Route>
+    </Switch>
+   </div>
+    
   );
 }
 
