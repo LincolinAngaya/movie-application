094557e71fed0c01093b299e6a5fa72e25122a68
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-27 15:34:19 +0300

    add new components

---
+++ src/components/Movie/MovieList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MovieItem from './MovieItem'
+function MovieList() {
+  return (
+    <div>
+      <>hello movie item</>
+    </div>
+  )
+}
+
+export default MovieList
--- src/components/Pages/AllMovies.jsx
+++ src/components/Pages/AllMovies.jsx
@@ -1,141 +1,14 @@
 import React from 'react'
 import SearchBox from '../Navigation/search'
 import poster from '../Pages/landing.jpg'
+import MovieList from '../Movie/MovieList'
 function AllMovies() {
   return (
     <>
      <SearchBox />
+     <MovieList />
    
-    <section id="section-b" class="grid">
-    <ul>
-      <li>
-        <div class="card">
-          <img src={poster} alt=""/>
-          <div class="card-content">
-            <h3 class="card-title">Mobile Application </h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque blanditiis ipsum enim inventore, eum repudiandae!</p>
-          </div>
-        </div>
-      </li>
-      <li>
-        <div class="card">
-          <img src={poster} alt=""/>
-          <div class="card-content">
-            <h3 class="card-title">Mobile Application </h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque blanditiis ipsum enim inventore, eum repudiandae!</p>
-          </div>
-        </div>
-      </li>
-      <li>
-        <div class="card">
-          <img src={poster} alt=""/>
-          <div class="card-content">
-            <h3 class="card-title">Mobile Application </h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque blanditiis ipsum enim inventore, eum repudiandae!</p>
-          </div>
-        </div>
-      </li>
-      <li>
-        <div class="card">
-          <img src={poster} alt=""/>
-          <div class="card-content">
-            <h3 class="card-title">Mobile Application </h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque blanditiis ipsum enim inventore, eum repudiandae!</p>
-          </div>
-        </div>
-      </li>
-      <li>
-        <div class="card">
-          <img src={poster} alt=""/>
-          <div class="card-content">
-            <h3 class="card-title">Mobile Application </h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque blanditiis ipsum enim inventore, eum repudiandae!</p>
-          </div>
-        </div>
-      </li>
-      <li>
-        <div class="card">
-          <img src={poster} alt=""/>
-          <div class="card-content">
-            <h3 class="card-title">Mobile Application </h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque blanditiis ipsum enim inventore, eum repudiandae!</p>
-          </div>
-        </div>
-      </li>
-      <li>
-        <div class="card">
-          <img src={poster} alt=""/>
-          <div class="card-content">
-            <h3 class="card-title">Mobile Application </h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque blanditiis ipsum enim inventore, eum repudiandae!</p>
-          </div>
-        </div>
-      </li>
-      <li>
-        <div class="card">
-          <img src={poster} alt=""/>
-          <div class="card-content">
-            <h3 class="card-title">Mobile Application </h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque blanditiis ipsum enim inventore, eum repudiandae!</p>
-          </div>
-        </div>
-      </li>
-      <li>
-        <div class="card">
-          <img src={poster} alt=""/>
-          <div class="card-content">
-            <h3 class="card-title">Mobile Application </h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque blanditiis ipsum enim inventore, eum repudiandae!</p>
-          </div>
-        </div>
-      </li>
-      <li>
-        <div class="card">
-          <img src={poster} alt=""/>
-          <div class="card-content">
-            <h3 class="card-title">Mobile Application </h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque blanditiis ipsum enim inventore, eum repudiandae!</p>
-          </div>
-        </div>
-      </li>
-      <li>
-        <div class="card">
-          <img src={poster} alt=""/>
-          <div class="card-content">
-            <h3 class="card-title">Mobile Application </h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque blanditiis ipsum enim inventore, eum repudiandae!</p>
-          </div>
-        </div>
-      </li>
-      <li>
-        <div class="card">
-          <img src={poster} alt=""/>
-          <div class="card-content">
-            <h3 class="card-title">Mobile Application </h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque blanditiis ipsum enim inventore, eum repudiandae!</p>
-          </div>
-        </div>
-      </li>
-      <li>
-        <div class="card">
-        <img src={poster} alt=""/>
-          <div class="card-content">
-            <h3 class="card-title">Tech Marketing</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque blanditiis ipsum enim inventore, eum repudiandae!</p>
-          </div>
-        </div>
-      </li>
-        <li>
-            <div class="card">
-            <img src={poster} alt=""/>
-                <div class="card-content">
-                  <h3 class="card-title">Web Development</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque blanditiis ipsum enim inventore, eum repudiandae!</p>
-                </div>
-            </div>
-        </li>
-    </ul>
-   </section>
+    
 </>
   )
 }
