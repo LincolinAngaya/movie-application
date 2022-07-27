16547ea2a03e8e6248c0caf635b595496b886608
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-27 10:54:25 +0300

    add paths

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import FavoriteMovies from './components/Pages/FavoriteMovies';
 import NewMovies from './components/Pages/NewMovies';
 function App() {
   return (
-    <div className="App">
-     <Route path='/' 
+    <div>
+     <Route path='/' >
+      <AllMovies />
+     </Route>
+     <Route path='/new-movies' >
+      <NewMovies />
+     </Route>
+     <Route path='/favorites' >
+      <FavoriteMovies/>
+     </Route>
     </div>
   );
 }
