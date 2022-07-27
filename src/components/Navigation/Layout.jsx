eec19c4563081461702c34091c92e6e2d457acb8
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-27 11:47:45 +0300

    add css for header

--- src/components/Navigation/Layout.jsx
+++ src/components/Navigation/Layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './layout.css'
 import { Link } from 'react-router-dom'
 
 function Layout() {
   return (
-    <header>
-        <div>MovieLink</div>
+    <header className='header'>
+        <div className='logo'>MovieLink</div>
         <nav>
             <ul>
                 <Link to='/'>Movies</Link>
