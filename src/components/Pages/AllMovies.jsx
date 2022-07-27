74fc627095f1e66a7cfb6f0098ca339527f80e94
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-27 13:35:03 +0300

    add entries in the movie

--- src/components/Pages/AllMovies.jsx
+++ src/components/Pages/AllMovies.jsx
@@ -11,16 +11,17 @@ function AllMovies() {
    <div className = "movie-info">
     <h3 className = "movie-title"></h3>
     <ul className = "movie-misc-info">
-        <li className = "year">Year: </li>
-        <li className = "rated">Ratings: </li>
+        <li className = "year">Year:2022</li>
+        <li className = "rated">Ratings: PG-13</li>
         <li className = "released">Released:</li>
     </ul>
-    <p className = "genre"><b>Genre:</b> </p>
+    <p className = "genre"><b>Genre:</b>Action,Adventure,Comedy </p>
     <p className = "writer"><b>Writer:</b> </p>
     <p className = "actors"><b>Actors: </b></p>
     <p className = "plot"><b>Plot:</b> </p>
-    <p className = "language"><b>Language:</b> \</p>
-    <p className = "awards"><b><i className = "fas fa-award"></i></b> </p>
+    <p className = "language"><b>Language:</b>English,French,Spanish</p>
+    <p className = "awards"><b><i className = "fas fa-award"></i></b> Nominated for Osacar</p>
+    <button>Add to Cart</button>
 </div>
 </div>
 </>
