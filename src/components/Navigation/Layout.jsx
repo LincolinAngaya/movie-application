48720ce6d2e94194377f30e23461371e20d11762
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-27 11:35:15 +0300

    add link to the navigation links

--- src/components/Navigation/Layout.jsx
+++ src/components/Navigation/Layout.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Layout() {
   return (
     <header>
         <div>MovieLink</div>
+        <nav>
+            <ul>
+                <Links></Links>
+            </ul>
+        </nav>
     </header>
   )
 }
