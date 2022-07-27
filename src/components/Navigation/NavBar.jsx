92b23e33000a1ab796d83e2f7799b9eda103b01a
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-27 12:33:27 +0300

    add searchbar component and jsx

--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -14,6 +14,7 @@ function NavBar() {
             </ul>
         </nav>
     </header>
+    
   )
 }
 
---
+++ src/components/Navigation/search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function search() {
+  return (
+    <div className='search-container'>
+    <div className="search-element">
+        <h3>Search Movie:</h3>
+        <input type="text" className="form-control" />
+    </div>
+</div>
+  )
+}
+
+export default search
