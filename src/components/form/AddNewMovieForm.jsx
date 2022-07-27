1aa6842afc43c30eb4386b89efd3912b7be852bb
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-27 15:05:10 +0300

    add form

--- src/components/form/AddNewMovieForm.jsx
+++ src/components/form/AddNewMovieForm.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
-import './AddNewMovieForm.css'
+import './addNewForm.css'
 function AddNewMovieForm() {
   return (
-    <div>AddNewMovieForm</div>
+    <div classname='Card'>
+    <form className='form'>
+    <div className='control'>
+            <label htmlFor='title'>Movie Number</label>
+            <input type="text"  required id="title"/>
+        </div>
+        <div className='control'>
+            <label htmlFor='title'>Movie Title</label>
+            <input type="text"  required id="title"/>
+        </div>
+        <div className='control'>
+            <label htmlFor='title'>Movie Type</label>
+            <input type="text"  required id="title"/>
+        </div>
+        <div className='control'>
+            <label htmlFor='title'>Movie Year</label>
+            <input type="text"  required id="title"/>
+        </div>
+
+        <div className='control'>
+            <label htmlFor='image'>Movie Image</label>
+            <input type="url"  required id="image"/>
+        </div>
+
+        
+ 
+        <div className='actions'>
+            <button>Add Movie</button>
+        </div>
+
+
+    </form>
+  </div>
   )
 }
 
