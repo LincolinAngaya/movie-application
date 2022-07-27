95afcbb38a40a7f51a117ad166d90ea2ecc0b362
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-27 12:45:20 +0300

    add search element styling

--- src/components/Navigation/search.jsx
+++ src/components/Navigation/search.jsx
@@ -4,7 +4,7 @@ function SearchBox() {
   return (
     <div className='search-container'>
     <div className="search-element">
-        <h3>Search Movie:</h3>
+
         <input type="text" className="form-control" />
     </div>
 </div>
