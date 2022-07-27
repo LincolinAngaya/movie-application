099152bb71ea91ee23b25c590dea6be6a5ed9d31
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-27 14:19:10 +0300

    remove the jsx

--- src/components/Pages/AllMovies.jsx
+++ src/components/Pages/AllMovies.jsx
@@ -5,26 +5,7 @@ function AllMovies() {
   return (
     <>
      <SearchBox />
-     <div className='result-container'>
-    <div className = "movie-poster">
-    <img src = {poster}alt = "movie poster"/>
-    </div>
-   <div className = "movie-info">
-    <h3 className = "movie-title"></h3>
-    <ul className = "movie-misc-info">
-        <li className = "year">Year:2022</li>
-        <li className = "rated">Ratings: PG-13</li>
-        <li className = "released">Released:</li>
-    </ul>
-    <p className = "genre"><b>Genre:</b>Action,Adventure,Comedy </p>
-    <p className = "writer"><b>Writer:</b> </p>
-    <p className = "actors"><b>Actors: </b></p>
-    <p className = "plot"><b>Plot:</b> </p>
-    <p className = "language"><b>Language:</b>English,French,Spanish</p>
-    <p className = "awards"><b><i className = "fas fa-award"></i></b> Nominated for Osacar</p>
-    <button>Add to Cart</button>
-</div>
-</div>
+    
 </>
   )
 }
