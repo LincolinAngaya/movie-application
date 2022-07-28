9158aa122fdfada2fd285109699babcec04ebebb
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-28 09:41:27 +0300

    add movi item details

--- src/components/Movie/MovieItem.jsx
+++ src/components/Movie/MovieItem.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import poster from '../Pages/landing.jpg'
 
 function MovieItem() {
   return (
-    <div>MovieItem</div>
+    <div className='wrapper grid'>
+      <div className='container'>
+        <div className='banner-img'>
+           <img src={poster} alt='poster' />
+           <h1 className='title'> The Post</h1>
+           <h1 className='year'> Action</h1>
+           <h1 className='year'> 2021</h1>
+             <button className='actions'>Pick Movie</button>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Movie/MovieList.jsx
+++ src/components/Movie/MovieList.jsx
@@ -3,6 +3,7 @@ import MovieItem from './MovieItem'
 function MovieList() {
   return (
     <div>
+      < MovieItem />
       <>hello movie item</>
     </div>
   )
