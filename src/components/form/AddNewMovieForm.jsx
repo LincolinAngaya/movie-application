922fbef1398bf2340a49ec394ba20b1a16f48a56
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-28 17:25:19 +0300

    add ref to the rest of the inputs

--- src/components/form/AddNewMovieForm.jsx
+++ src/components/form/AddNewMovieForm.jsx
@@ -3,11 +3,20 @@ import {useRef} from 'react'
 import './addNewForm.css'
 function AddNewMovieForm() {
     const imageInputRef = useRef();
+    const titleInputRef = useRef();
+    const typeInputRef = useRef();
+    const yearInputRef = useRef();
+    const trailerInputRef = useRef();
+   
 
     function submitHandler(e){
            e.preventDefault();
 
             const enteredImage = imageInputRef.current.value;
+            const enteredTitle = titleInputRef.current.value;
+            const enteredType = typeInputRef.current.value;
+            const enteredYear = yearInputRef.current.value;
+            const enteredTrailer = trailerInputRef.current.value;
          
     }
   return (
@@ -19,19 +28,19 @@ function AddNewMovieForm() {
         </div>
     <div className='control'>
             <label htmlFor='title'>Movie Title</label>
-            <input type="text"  required id="title"/>
+            <input type="text"  required id="title" ref={titleInputRef}/>
         </div>
         <div className='control'>
             <label htmlFor='type'>Movie Type</label>
-            <input type="text"  required id="type"/>
+            <input type="text"  required id="type" ref={typeInputRef}/>
         </div>
         <div className='control'>
             <label htmlFor='year'>Movie Year</label>
-            <input type="text"  required id="year"/>
+            <input type="text"  required id="year" ref={yearInputRef}/>
         </div>
         <div className='control'>
             <label htmlFor='description'>Movie Trailer</label>
-            <textarea id="description" required rows='5' />
+            <textarea id="description" required rows='5'ref={trailerInputRef} />
         </div>
         <div className='actions'>
             <button>Add Meetup</button>
