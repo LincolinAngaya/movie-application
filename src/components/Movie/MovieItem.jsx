6d56d89815ca02c3ad64a1ff0f21d51f3177de17
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-28 10:15:55 +0300

    add the content clasname

--- src/components/Movie/MovieItem.jsx
+++ src/components/Movie/MovieItem.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import poster from '../Pages/landing.jpg'
-import '../Movie/MovieItem.css'
+import './MovieItem.css'
 
 function MovieItem() {
   return (
     <div className='wrapper grid'>
       <div className='container'>
-        <div className='banner-img'>
-           <img src={poster} alt='poster' />
+      {/* <div className='banner-img'>
+           <img   src={poster} alt='poster' />
+         </div> */}
+         <div className='content'>
            <h1 className='title'> The Post</h1>
            <h1 className='year'> Action</h1>
            <h1 className='year'> 2021</h1>
@@ -15,9 +17,11 @@ function MovieItem() {
            <p className='description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem nesciunt quibusdam voluptate, debitis rem illo similique ipsa aliquid cumque quidem suscipit quos dolores. Tenetur quam asperiores </p>
           <button className='actions'>Pick Movie</button>
 
-        </div>
-      </div>
-    </div>
+          </div>
+        
+          </div>
+          </div>
+     
   )
 }
 
