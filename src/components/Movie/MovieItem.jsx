b9d487de9462339abc6cc8527933c9eda0a5b2fa
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-28 12:03:04 +0300

    remove the initial styling

--- src/components/Movie/MovieItem.jsx
+++ src/components/Movie/MovieItem.jsx
@@ -78,7 +78,7 @@ function MovieItem() {
           <h1 className='title'> Movie Title:<span>The Post</span></h1>
      <h1 className='year'>Movie Type:<span> Action</span></h1>
      <h1 className='year'>Movie Year: <span>2021</span></h1>
-     <p className='description'>Movie Trailr:<span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem nesciunt quibusdam voluptate, debitis rem illo similique ipsa aliquid cumque quidem suscipit quos dolores. Tenetur quam asperiores</span> </p>
+     <h1 className='description'>Movie Trailer:<span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem nesciunt quibusdam voluptate, debitis rem illo similique ipsa aliquid cumque quidem suscipit quos dolores. Tenetur quam asperiores</span> </h1>
      <button className='actions'>Pick Movie</button>
           </div>
         </div>
