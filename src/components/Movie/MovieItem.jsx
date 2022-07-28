cbbd9df243c7fb396a6ed0af33cf4b98cdac25bd
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-28 10:32:23 +0300

    add font types

--- src/components/Movie/MovieItem.jsx
+++ src/components/Movie/MovieItem.jsx
@@ -13,13 +13,44 @@ function MovieItem() {
            <h1 className='title'> Movie Title:<span>The Post</span></h1>
            <h1 className='year'>Movie Type:<span> Action</span></h1>
            <h1 className='year'>Movie Year: <span>2021</span></h1>
-           <h1 className='year'> Movie Producers:tyler perry mike tyson, vandebeek<span></span></h1>
+           <h1 className='year'> Movie Producers:<span>tyler perry mike tyson, vandebeek</span></h1>
            <h1 className='description'>Movie Trailr:<span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem nesciunt quibusdam voluptate, debitis rem illo similique ipsa aliquid cumque quidem suscipit quos dolores. Tenetur quam asperiores</span> </h1>
           <button className='actions'>Pick Movie</button>
 
           </div>
         
           </div>
+          <div className='container'>
+      {/* <div className='banner-img'>
+           <img   src={poster} alt='poster' />
+         </div> */}
+         <div className='content'>
+           <h1 className='title'> Movie Title:<span>The Post</span></h1>
+           <h1 className='year'>Movie Type:<span> Action</span></h1>
+           <h1 className='year'>Movie Year: <span>2021</span></h1>
+           <h1 className='year'> Movie Producers:<span>tyler perry mike tyson, vandebeek</span></h1>
+           <h1 className='description'>Movie Trailr:<span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem nesciunt quibusdam voluptate, debitis rem illo similique ipsa aliquid cumque quidem suscipit quos dolores. Tenetur quam asperiores</span> </h1>
+          <button className='actions'>Pick Movie</button>
+
+          </div>
+        
+          </div>
+          <div className='container'>
+      {/* <div className='banner-img'>
+           <img   src={poster} alt='poster' />
+         </div> */}
+         <div className='content'>
+           <h1 className='title'> Movie Title:<span>The Post</span></h1>
+           <h1 className='year'>Movie Type:<span> Action</span></h1>
+           <h1 className='year'>Movie Year: <span>2021</span></h1>
+           <h1 className='year'> Movie Producers:<span>tyler perry mike tyson, vandebeek</span></h1>
+           <h1 className='description'>Movie Trailr:<span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem nesciunt quibusdam voluptate, debitis rem illo similique ipsa aliquid cumque quidem suscipit quos dolores. Tenetur quam asperiores</span> </h1>
+          <button className='actions'>Pick Movie</button>
+
+          </div>
+        
+          </div>
+
           </div>
      
   )
