05ebed616fcac12d57ed5a67e60e286ec6388ec4
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-28 17:54:15 +0300

    add fetch url

--- src/components/Pages/NewMovies.jsx
+++ src/components/Pages/NewMovies.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import AddNewMovieForm from '../form/AddNewMovieForm'
 function NewMovies() {
+  
+  function addMovieHandler(movieDetails){
+       fetch('https://phase-2-project-599c2-default-rtdb.firebaseio.com/')
+  }
   return (
     <section>
     <h1>Add New Movie</h1>
-    <AddNewMovieForm />
+    <AddNewMovieForm  onAddMovie={addMovieHandler}/>
     </section>
   )
 }
