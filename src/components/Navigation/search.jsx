3ba19cfba6bf973f3c6676ac188f81ddfa000f15
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-29 08:34:25 +0300

    add state to search input

--- src/components/Navigation/search.jsx
+++ src/components/Navigation/search.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
 import './nav.css'
 function SearchBox() {
+  const [query, setQuery]=useState('');
+
+  const search = (data) =>{
+    return data.filter((movie) => movie.Title.toLowerCase().include(query));
+  }
   return (
     <div className='search-container'>
     <div className="search-element">
-        <input type="text" className="form-control" />
+        <input type="text" 
+         className="form-control"
+         onChange={(e) => setQuery(e.target.value)} 
+         />
     </div>
 </div>
   )
